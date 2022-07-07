c531139302803f3efd624fd71279e53c3b7c6601
PIYUSH_MISHRA <65788721+PIYUSH-MISHRA-00@users.noreply.github.com>
2022-07-07 16:32:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.birthdaygreet
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            hideSystemUIAndNavigation(this)
+        }
+    }
+
+    private fun hideSystemUIAndNavigation(activity: Activity) {
+        val decorView: View = activity.window.decorView
+        decorView.systemUiVisibility =
+            (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+
+    fun createBirthdayCard(view: View) {
+
+        val name1 = nameInput.editableText.toString()
+
+        val intent = Intent(this, BirthdayGreetingActivity::class.java)
+
+        intent.putExtra(BirthdayGreetingActivity.NAME_EXTRA, name1)
+
+        startActivity(intent)
+
+        val name = nameInput.editableText.toString()
+
+        Toast.makeText(this,"Happy Birthday😄 $name✨", Toast.LENGTH_LONG).show()
+    }
+}
